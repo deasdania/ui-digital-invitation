d020ab2a29e5d0623f2144c92d022e9ced2b7f89
Pule <pule@Pules-MacBook-Pro.local>
2022-01-16 20:14:00 +0700

    initial commit

---
+++ pages/login/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Layout from "../../components/Layout";
+
+const Home: NextPage = () => {
+    return (
+      <Layout title="Home">
+        <Header />
+        <main>
+
+        </main>
+        {/* <Footer /> */}
+      </Layout>
+    );
+  };
+  
+  export default Home;
+  
